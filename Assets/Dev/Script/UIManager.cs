5f83224a83b7f8f4fa57c0d69188dcf760c813d7
Burak <90563983+burakgulirmak35@users.noreply.github.com>
2023-08-03 01:44:00 +0300

    ++

--- Assets/Dev/Script/UIManager.cs
+++ Assets/Dev/Script/UIManager.cs
@@ -1,9 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI txt_Goal;
+    [SerializeField] private TextMeshProUGUI txt_Score;
+    [SerializeField] private TextMeshProUGUI txt_Moves;
+
+    [SerializeField] private GameObject panel_Settings;
+
+    private void Awake()
+    {
+
+    }
+
+    public void btn_Settings()
+    {
+
+        panel_Settings.SetActive(true);
+
+    }
 
 }
